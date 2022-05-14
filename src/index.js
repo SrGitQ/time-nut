d1c64097914a16e162b81373cc49e7da27bb1b82
JMBalanzar <jmbalanzar@gmail.com>
2022-05-14 04:14:16 -0500

    main files added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const App = () => {
+  const [hours, setHours] = useState(1);
+  const [minutes, setMinutes] = useState(59);
+  const [seconds, setSeconds] = useState(60);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  if (seconds === 0) {
+    setSeconds(59);
+    setMinutes(minutes - 1);
+  }
+  if (minutes === 0) {
+    setMinutes(59);
+    setHours(hours - 1);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useEffect(() => {
+    seconds > 0 && setTimeout(() => setSeconds(seconds - 1), 1000);
+  }, [seconds]);
+  
+  return (
+    <div className="bg-black flex h-screen">
+      <div className="m-auto">
+        <h1 className="text-8xl font-bold text-red-800 text-center align-middle">
+          {hours < 10 ? "0" + hours : hours}:
+          {minutes < 10 ? "0" + minutes : minutes}:
+          {seconds < 10 ? "0" + seconds : seconds}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
